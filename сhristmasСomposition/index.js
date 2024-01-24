af0416d476d3d43d816999097d46cac6fdb83bba
Anna Trifonova <3809322999@ukr.net>
2024-01-24 21:51:13 +0200

    Add preloader

--- сhristmasСomposition/index.js
+++ сhristmasСomposition/index.js
@@ -3,6 +3,9 @@ import animateSnowflakes from './snow.js';
 
 const formEl = document.querySelector('.form');
 const titleEl = document.querySelector('.title');
+const preloaderEl = document.querySelector('.preloader');
+const bodyBackground = new Image();
+bodyBackground.src = '../assets/winter-background.svg';
 
 let clicked = false;
 let inputText = '';
@@ -27,8 +30,16 @@ function animate() {
 
 	requestAnimationFrame(animate);
 }
+animate();
 
-window.addEventListener('load', animate);
+bodyBackground.onload = () => {
+	animate();
+	preloaderEl.className = 'preloader hidden-loader';
+
+	setTimeout(() => {
+		preloaderEl.className = 'hidden';
+	}, 5000);
+};
 
 formEl[0].addEventListener('input', event => {
 	inputText = event.target.value;
